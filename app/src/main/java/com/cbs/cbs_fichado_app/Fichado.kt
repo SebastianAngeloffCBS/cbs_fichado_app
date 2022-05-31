bdfed5ac10a5e20d9e4b796128bd5d4a4a68cf42
DesarrolladorGris <angeloffgonzalo@gmail.com>
2022-05-31 06:37:06 -0300

    Ultimas modificaciones martes 31052022

--- app/src/main/java/com/cbs/cbs_fichado_app/Fichado.kt
+++ app/src/main/java/com/cbs/cbs_fichado_app/Fichado.kt
@@ -1,33 +1,42 @@
 package com.cbs.cbs_fichado_app
 
-import android.app.Dialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.text.InputType
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.cbs.cbs_fichado_app.databinding.ActivityFichadoBinding
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.google.android.material.navigation.NavigationView
 import com.google.zxing.integration.android.IntentIntegrator
+import org.json.JSONException
+import java.util.*
+import kotlin.collections.HashMap
+
 
 class Fichado : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityFichadoBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,8 +47,8 @@ class Fichado : AppCompatActivity() {
         val bundle = intent.extras
         val dato = bundle?.getString("UsuarioIntra")
 
-        val txtUsuario = findViewById<TextView>(R.id.txtUsuario)
-        txtUsuario.setText(dato)
+//        val txtUsuario = findViewById<TextView>(R.id.txtUsuario)
+//        txtUsuario.setText(dato)
 
         setSupportActionBar(binding.appBarFichado.toolbar)
 
@@ -53,6 +62,10 @@ class Fichado : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -154,9 +167,84 @@ class Fichado : AppCompatActivity() {
     fun validaPersonal(dni : String){
 
         //Me conecto al servidor para validar la persona
+        val url = "http://ws.grupocbs.com.ar/api/Fichado/VerificaDocumentoUsuario"
 
+        val queue = Volley.newRequestQueue(this)
+
+        val request: StringRequest = object : StringRequest(
+            Method.POST,
+            url,
+            Response.Listener { response ->
+                try
+                {
+                   val mapper = jacksonObjectMapper()
+
+                    val userData: Personal = mapper.readValue(response)
+
+                    if (userData.codemp != "0")
+                    {
+//                        guardarDatos(userData)
+//
+//                        val fichado = Intent(this, Fichado::class.java)
+//
+//                        fichado.putExtra("UsuarioIntra", userData.usuario)
+//
+//                        startActivity(fichado)
+                        Toast.makeText(this, userData.nombreapellido, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    else
+                    {
+                        var progressBar = findViewById<ProgressBar>(R.id.progressBar)
+                        progressBar.isVisible = false
+                        var boton = findViewById<Button>(R.id.btn_conectar)
+                        boton.isVisible = true
+
+
+                        Toast.makeText(this, "DNI INCORRECTO", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                }
+                catch (e: JSONException)
+                {
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener { error -> // method to handle errors.
+                Toast.makeText(
+                    this,
+                    "Fail to get response = $error",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }) {
+            override fun getParams(): Map<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+
+                params["dni"] = dni
+
+                return params
+            }
+        }
+        queue.add(request)
 
     }
+
+    fun soporte(item: MenuItem) {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("ATENCIÓN")
+        builder.setMessage("Ante cualquier inconveniente comunicarse con el departamento de sistemas. Gracias")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        builder.setNegativeButton("Entendido"){dialogInterface, which ->
+            Toast.makeText(applicationContext,"Gracias", Toast.LENGTH_LONG).show()
+        }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
+
+    }
+
 
 
 
---
+++ app/src/main/java/com/cbs/cbs_fichado_app/Personal.kt
@@ -0,0 +1,9 @@
+package com.cbs.cbs_fichado_app
+
+data class Personal(
+    val codemp : String ? ,
+    val legajo : String ?,
+    val nombreapellido: String ?,
+    val dni  : String?,
+    val dimension  : String?
+    )
